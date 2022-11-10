d7430fa29958fde07b977703e2e191eed1dfaca4
hoefkensj <hoefkens.j@gmail.com>
2022-11-10 03:27:08 +0100

    Update icon0.svg, icon1.svg, and myPyQtTest.py

--- myPyQtTest.py
+++ myPyQtTest.py
@@ -5,10 +5,10 @@ from myPyQt.lib import QtWgt,tree,elements,gui
 
 GUI=gui.Gui()
 
-GUI['Elements'] |= elements.iBtn(n='Search')
-GUI['Elements'] |= elements.iBtn(n='Edit')
+GUI['Elements'] |= elements.iBtn('Search')
+GUI['Elements'] |= elements.iBtn('Edit',lbl='test')
 GUI['Elements'] |= QtWgt.make(n='txt_Tree')
-GUI['Elements'] |= elements.chkBox(n='RegEx')
-GUI['Elements'] |= QtWgt.make(n='wgt_RegEx2')
+# GUI['Elements'] |= elements.chkBox('RegEx')
+GUI['Elements'] |= QtWgt.make(n='chk_RegEx',)
 GUI['Elements'] |= QtWgt.make(n='trw_Tree')
 GUI['Run']()
